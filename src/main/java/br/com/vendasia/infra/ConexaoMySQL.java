d5418260324c44fd202694aa3ef50eeb00dccd72
Douglas Silva <douglasitpro@gmail.com>
2026-05-07 23:03:44 -0300

    Aplicando arquivo de configuração para conexão com mysql

--- src/main/java/br/com/vendasia/infra/ConexaoMySQL.java
+++ src/main/java/br/com/vendasia/infra/ConexaoMySQL.java
@@ -1,5 +1,20 @@
 package br.com.vendasia.infra;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexaoMySQL {
-    
+
+    private static final Dotenv ENV = Dotenv.load();
+
+    public static Connection obter() throws SQLException {
+        String url = ENV.get("DB_URL");
+        String user = ENV.get("DB_USER");
+        String pass = ENV.get("DB_PASS");
+
+        return DriverManager.getConnection(url, user, pass);
+    }
+
 }
