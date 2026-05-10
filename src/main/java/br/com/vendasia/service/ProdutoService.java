34fac6108b50f49eeeae6d33c58d5dac7007c184
Douglas Silva <douglasitpro@gmail.com>
2026-05-10 11:08:52 -0300

    ProdutoService: Adicionada funções de regra de negocio para conexão com ProdutoRepository e Banco de dados

--- src/main/java/br/com/vendasia/service/ProdutoService.java
+++ src/main/java/br/com/vendasia/service/ProdutoService.java
@@ -7,10 +7,61 @@ import br.com.vendasia.model.Produto;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
 
 
 public class ProdutoService {
 
-    
+        // Listar produtos
+        public List<Produto> listarTodos() throws SQLException {
+            try (Connection conn = ConexaoMySQL.obter()) {
+                return new ProdutoRepository(conn).buscarTodos();
+            }
+        }
+
+        // Buscar produto por ID
+        public Optional<Produto> buscarPorId(int id) throws SQLException {
+            try (Connection conn = ConexaoMySQL.obter()) {
+                return new ProdutoRepository(conn).buscarPorId(id);
+            }
+        }
+
+        //Buscar Por Categoria
+        public List<Produto> buscarPorCategoria(String categoria) throws SQLException {
+            if (categoria == null || categoria.isBlank()) {
+                throw new IllegalArgumentException("Categoria não pode ser vazia");
+            } 
+            try (Connection conn = ConexaoMySQL.obter()) {
+                return new ProdutoRepository(conn).buscarPorCategoria(categoria);
+            }
+
+        // Cadastrar um produto
+        public Produto cadastrar(Produto produto) throws SQLException {
+            if (produto.nome() == null || produto.nome().isBlank())
+                throw new IllegalArgumentException("Nome do produto é obrigatório.");
+            if (produto.sku() == null || produto.sku().isBlank())
+                throw new IllegalArgumentException("SKU é obrigatório.");
+            if (produto.precoVenda() == null || produto.precoVenda().signum() <=0 )
+                throw new IllegalArgumentException("Preço de venda de ser maior que zero.");
+
+            try (Connection conn = ConexaoMySQL.obter()) {
+                return new ProdutoRepository(conn).inserir(produto);
+            }
+        }
+
+        // Tops mais vendidos
+        public List<String> topMaisVendidos(int limite) throws SQLException {
+            if (limite <= 0) throw new IllegalArgumentException("Limite dever ser maior que zero.");
+            try (Connection conn = ConexaoMySQL.obter()) {
+                return new ProdutoRepository(conn).topProdutosMaisVendidos(limite);
+            }
+        }
+
+        // Listar produto com estoque minimo
+        public List<Produto> estoqueCritico(int minimo) throws SQLException {
+            try (Connection conn = ConexaoMySQL.obter()){
+                return new ProdutoRepository(conn).buscarEstoqueCritico(minimo);
+            }
+        }
 
 }
