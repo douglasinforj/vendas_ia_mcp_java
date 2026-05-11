deb2201837e31680bcdb6b3090552626e9867b91
Douglas Silva <douglasitpro@gmail.com>
2026-05-10 21:21:11 -0300

    Refact: Teste adicionando uma transação de Pedido

--- src/main/java/br/com/vendasia/Main.java
+++ src/main/java/br/com/vendasia/Main.java
@@ -2,6 +2,9 @@ package br.com.vendasia;
 
 import br.com.vendasia.service.ClienteService;
 import br.com.vendasia.service.ProdutoService;
+import br.com.vendasia.service.PedidoService;
+
+import br.com.vendasia.model.ItemPedido;
 
 import java.math.BigDecimal;
 import java.sql.SQLException;
@@ -13,6 +16,7 @@ public class Main {
 
         ClienteService clienteService = new ClienteService();
         ProdutoService produtoService = new ProdutoService();
+        PedidoService pedidoService = new PedidoService();
 
 
         try {
@@ -24,6 +28,15 @@ public class Main {
             // Teste: Estoque crítico
             System.out.println("\n=== Produtos - Estoque Crítico ===");
             produtoService.topMaisVendidos(5).forEach(System.out::println);
+
+            // Teste: registrar pedido com transação
+            System.out.println("\n==== Registrando Pedido=====");
+            List<ItemPedido> itens = List.of(
+                new ItemPedido(null, 1, 2, BigDecimal.ZERO),  // produto 1, qtd 2
+                new ItemPedido(null, 2, 1, BigDecimal.ZERO)   // produto 2, qtd 1
+            );
+            pedidoService.registrarPedido(1, itens, "PIX", 1);
+            
             
         
         } catch(SQLException e){
